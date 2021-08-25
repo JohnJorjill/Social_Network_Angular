16409edbfc94c17077ad656882bad1d5fdfb02c8
Bat-Ulzii Orgil <orgil9506@gmail.com>
2021-08-25 17:22:22 -0500

    register login posts done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,53 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login.component';
+import {NgxWebstorageModule} from 'ngx-webstorage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { TokenInterceptor } from './token-interceptor';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PostTileComponent } from './shared/post-tile/post-tile.component';
+import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
+    HomeComponent,
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignupComponent,
+    LoginComponent,
+    PostTileComponent,
+    VoteButtonComponent,
+    SideBarComponent,
+    SubredditSideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxWebstorageModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FontAwesomeModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/post-tile/post-tile.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PostModel } from '../post-model';
+import { PostService } from '../post.service';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-post-tile',
+  templateUrl: './post-tile.component.html',
+  styleUrls: ['./post-tile.component.css']
+})
+export class PostTileComponent implements OnInit {
+
+  @Input() data: Array<PostModel>;
+
+  faComments = faComments;
+
+  posts$: Array<PostModel>;
+
+  constructor(private postService: PostService) { 
+    this.postService.getAllPosts().subscribe(post => {
+      this.posts$ = post;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
